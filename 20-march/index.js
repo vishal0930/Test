89ea27172ea13400472835fbe8e2c675b81ead4e
vishal0930 <vishal09121998@gmail.com>
2022-03-27 18:42:05 +0530

    Test File

---
+++ 20-march/index.js
@@ -0,0 +1,9 @@
+
+for(var n = 1; n <= 100; n++) {
+    if (n % 3 == 0 && n % 5 == 0)
+        console.log("fizzbuzz")
+    else if (n % 3 == 0)
+        console.log("fizz")
+    else if (n % 5 == 0)
+        console.log("buzz")   
+}
